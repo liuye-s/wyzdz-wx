7567de1c088fe9f1d13cb8f9a0305800ab69a9c0
shimolin <1625409694@qq.com>
2020-10-22 16:52:30 +0800

    个人信息编辑

--- pages/my/account/account.js
+++ pages/my/account/account.js
@@ -8,8 +8,8 @@ Page({
     region: ['北京市','天津市','上海市','重庆市','河北省','山西省','辽宁省','吉林省','黑龙江省','江苏省','浙江省','安徽省','福建省','江西省','山东省','河南省','湖北省','湖南省','广东省','海南省','四川省','贵州省','云南省','陕西省','甘肃省','青海省','台湾省','内蒙古自治区','广西壮族自治区','西藏自治区','宁夏回族自治区','新疆维吾尔自治区','香港特别行政区','澳门特别行政区'],
     nickName:'',
     avatarUrl: '',
-    textareaAValue: '',
-    textareaBValue: ''
+    textareaValue: '',
+    isChange:false,
   },
   attached() {
     let that = this;
@@ -22,31 +22,33 @@ Page({
       })
     }
   },
+  InputChange(e){
+    this.setData({
+      nickName: e.detail.value,
+      isChange:true
+    })
+  },
   PickerChange(e) {
     console.log(e);
     this.setData({
-      index: e.detail.value
-    })
-  },
-  MultiChange(e) {
-    this.setData({
-      multiIndex: e.detail.value
+      index: e.detail.value,
+      isChange:true
     })
   },
   RegionChange: function(e) {
     this.setData({
-      region: e.detail.value
+      region: e.detail.value,
+      isChange:true
     })
   },
-  textareaAInput(e) {
+  textareaInput(e) {
     this.setData({
-      textareaAValue: e.detail.value
+      textareaBValue: e.detail.value,
+      isChange:true
     })
   },
-  textareaBInput(e) {
-    this.setData({
-      textareaBValue: e.detail.value
-    })
+  saveUserInfo:function(){
+    console.log('保存用户信息');
   },
   onLoad:function(){
     this.attached();
